2a3028ae0a5734516e08a54760a2975489c16a9c
Shabalakko <bruno.fabio.1997@gmail.com>
2025-02-19 09:03:36 +0100

    Main Menu setup + fixes
    D'ora in poi switcho al pc DELL a lavoro

---
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenuCanvas;  // Canvas del menu principale
+    public GameObject optionsCanvas;   // Canvas delle opzioni di gioco
+
+    // --- 1. Play Button ---
+    public void PlayGame()
+    {
+        // Carica la scena di gioco
+        SceneManager.LoadScene("GyroLevel");
+    }
+
+    // --- 2. Options Button ---
+    public void OpenOptions()
+    {
+        // Mostra il Canvas delle opzioni
+        optionsCanvas.SetActive(true);
+
+        // Nasconde il Canvas del menu principale
+        mainMenuCanvas.SetActive(false);
+    }
+
+    // --- 3. Exit Button ---
+    public void ExitGame()
+    {
+        // Chiude l'applicazione su Android e PC
+        Application.Quit();
+
+        // Ferma l'editor di Unity (solo per test)
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    // --- Torna al Menu Principale dalle Opzioni ---
+    public void BackToMenu()
+    {
+        // Mostra il Canvas del menu principale
+        mainMenuCanvas.SetActive(true);
+
+        // Nasconde il Canvas delle opzioni
+        optionsCanvas.SetActive(false);
+    }
+}
