e23ddd3c9a43b88aaf62931265dec30eaba584f9
Shabalakko <bruno.fabio.1997@gmail.com>
2025-06-11 15:47:00 +0200

    final for real

--- Assets/Script/MainMenu/LoadSceneCredits.cs
+++ Assets/Script/MainMenu/LoadSceneCredits.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class LoadSceneButton : MonoBehaviour
+public class LoadSceneCredits : MonoBehaviour
 {
     
     void LoadScene()
     {
         Time.timeScale = 1;
         // Carica la scena specificata
-        SceneManager.LoadScene("LoadingScene"); 
+        SceneManager.LoadScene("MainMenu"); 
     }
 
 }
